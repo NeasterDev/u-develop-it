88cc967c25071dfb46d5c08646e7f3cf3213fd35
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-03 17:09:12 -0800

    added mysql database to server file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 app.get('/', (req, res) => {
     res.json({
@@ -16,6 +17,23 @@ app.get('/', (req, res) => {
 // Default respopnse for any other request (NOT FOUND)
 app.use((req, res) => {
     res.status(404).end();
+});
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mysql username
+        user: 'root',
+        // mysql password
+        password: 'N@sus&Oreo1',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 app.listen(PORT, () => {

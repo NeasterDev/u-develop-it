279c4854edd3885b37ce4a3ebad89648a416f1d3
Nicholas Easter <nicholaseaster@Nicholass-MacBook-Air.local>
2022-02-09 10:57:27 -0800

    added env file with db info

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
   host: 'localhost',
   // Your MySQL username,
-  user: 'root',
+  user: process.env.DB_USER,
   // Your MySQL password
-  password: '',
-  database: 'election'
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = db;

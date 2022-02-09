606adebb3494de006ddfb2d8f6d5be14446b6b26
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-09 12:13:09 -0800

    fixed conflicts

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,8 +5,4 @@ router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-module.exports = router;
->>>>>>> d60b0a6dc860090513795b00598461dd7364edda
